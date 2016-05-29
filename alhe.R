e692b3dfeafec9b2895934e86c91bddb4326b027
Alexis Nowikowski <nowikowski22@hotmail.com>
2016-05-29 21:37:24 +0200

    Funkcja celu - initial implementation

--- alhe.R
+++ alhe.R
@@ -9,9 +9,38 @@ initialize_ALHE <- function()
 {
   importMeals()
   mealsPerDay <<- 5
+  optimalCarbohydrates <<- 250
+  optimalProteins <<- 200
+  optimalFats <<- 80
 }
 
 initialize_ALHE()
+
+sumBySelector <- function(point, selector, count)
+{
+  sum <- 0
+  for (i in 1:count)
+  {
+    sum <- sum + point[[i]][selector]
+  }
+  
+  return(sum)
+}
+
+sumCarbohydrates <- function(point)
+{
+  return(sumBySelector(point, "Carbohydrates", mealsPerDay))
+}
+
+sumProteins <- function(point)
+{
+  return(sumBySelector(point, "Proteins", mealsPerDay))
+}
+
+sumFats <- function(point)
+{
+  return(sumBySelector(point, "Fats", mealsPerDay))
+}
 
 generateRandomPoint <- function()
 {
@@ -30,7 +59,7 @@ generateRandomPoint <- function()
 stopConditionFunc <- function(point)
 {
   #Przerywamy, jeżeli współczynnik dopasowania punktu osiągnie daną wartość
-  return(evaluateFunc(point) > 0.9)
+  return(evaluateFunc(point) > 0.8)
 }
 
 neighborHoodFunc <- function(point)
@@ -64,9 +93,20 @@ neighborHood <- neighborHoodFunc(randomPoint)
 
 evaluateFunc <- function(point)
 {
-  #TODO
-  return(1)
+  deltaCarbohydrates <- abs(optimalCarbohydrates - sumCarbohydrates(point))
+  deltaProteins <- abs(optimalProteins - sumProteins(point))
+  deltaFats <- abs(optimalFats - sumFats(point))
+  deltaSum <- deltaCarbohydrates + deltaProteins + deltaFats
+  
+  if (deltaSum == 0)
+  {
+    return(1)
+  }
+  
+  return(1/((deltaCarbohydrates + deltaProteins + deltaFats)/100))
 }
 
-#result = tabuSearch(10, generateRandomPoint(), stopConditionFunc, neighborHoodFunc, evaluateFunc)
-#print(result)
+result = tabuSearch(10, generateRandomPoint(), stopConditionFunc, neighborHoodFunc, evaluateFunc)
+print(paste("Carbohydrates wanted: ", optimalCarbohydrates, ". Got: ", sumCarbohydrates(result), sep = ''))
+print(paste("Proteins wanted: ", optimalProteins, ". Got: ", sumProteins(result), sep = ''))
+print(paste("Fats wanted: ", optimalFats, ". Got: ", sumFats(result), sep = ''))
