526294e6c06b39ed96cb50e9577cc0671a620aff
Alexis Nowikowski <nowikowski22@hotmail.com>
2016-05-29 20:05:22 +0200

    Implementation of generateRandomPoint and neighborHoodFunc

--- alhe.R
+++ alhe.R
@@ -8,9 +8,24 @@ importMeals <- function()
 initialize_ALHE <- function()
 {
   importMeals()
+  mealsPerDay <<- 5
 }
 
 initialize_ALHE()
+
+generateRandomPoint <- function()
+{
+  randomPoint <- list()
+  mealsSize <- nrow(meals)
+  for (mealNo in 1:mealsPerDay)
+  {
+    randomMealIndex <- sample(1:mealsSize, 1)
+    randomMeal <- meals[randomMealIndex, ]
+    randomPoint <- append(randomPoint, list(randomMeal))
+  }
+  
+  return(randomPoint)
+}
 
 stopConditionFunc <- function(point)
 {
@@ -20,15 +35,38 @@ stopConditionFunc <- function(point)
 
 neighborHoodFunc <- function(point)
 {
-  #TODO
-  return(c(point-0.01, point-0.05, point+0.01, point+0.014, point+0.05))
+  mealsSize <- nrow(meals)
+  
+  neighborHoodSize <- mealsPerDay*(mealsSize-1)
+  neighborHoodElementsAdded <- 0
+  neighborHood <- vector(mode = "list", length = neighborHoodSize)
+  
+  for (mealNo in 1:mealsPerDay)
+  {
+    for (selectedMealIndex in 1:mealsSize)
+    {
+      selectedMeal <- meals[selectedMealIndex, ]
+      if (!identical(selectedMeal["MealName"], point[[mealNo]]["MealName"]))
+      {
+        neighbor <- point
+        neighbor[mealNo] <- list(selectedMeal)
+        neighborHood[neighborHoodElementsAdded] <- neighbor
+        neighborHoodElementsAdded <- neighborHoodElementsAdded + 1
+      }
+    }
+  }
+  
+  return(neighborHood)
 }
+
+randomPoint <- generateRandomPoint()
+neighborHood <- neighborHoodFunc(randomPoint)
 
 evaluateFunc <- function(point)
 {
   #TODO
-  return(point)
+  return(1)
 }
 
-result = tabuSearch(10, 0, stopConditionFunc, neighborHoodFunc, evaluateFunc)
-print(result)
+#result = tabuSearch(10, generateRandomPoint(), stopConditionFunc, neighborHoodFunc, evaluateFunc)
+#print(result)
